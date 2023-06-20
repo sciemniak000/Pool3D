18e809398daa3012d8462b86b0b163097fed5819
Przemysław Indyka <sciemniak000@gmail.com>
2023-06-20 02:20:30 +0200

    Failed attempt to run collisions with walls

--- Node3DBall.gd
+++ Node3DBall.gd
@@ -1,22 +1,36 @@
 class_name Ball extends Node3D
 
 var speed
+var futurePosition
+var inMovement := false
+
+var allWalls
+var wallsGettingNearer := []
+var distancesFromWalls := [-1., -1., -1.]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	speed = Vector3(0., 0., 0.)
-	
-	var timer := Timer.new()
-	add_child(timer)
-	
-	timer.wait_time = 3.0
-	timer.one_shot = true
-	timer.connect("timeout", enableMovement)
-	timer.start()
 
-func enableMovement():
-	speed = Vector3(-1., -10., 0.)
+func init(positioning, walls):
+	position = positioning
+	allWalls = walls
 
+func setMovement(newSpeed, walls):
+	speed = newSpeed
+	wallsGettingNearer = walls
+	inMovement = true
+
+func countNewPosition(delta):
+	var position_delta := Vector3(delta * speed[0], delta * speed[1], delta * speed[2])
+	futurePosition = position + position_delta
+
+func countWallDistances():
+	for i in 3:	
+		distancesFromWalls[i] = countFutureDistanceToWall(wallsGettingNearer[i])
+
+func countFutureDistanceToWall(wall):
+	return wall.normalVector.dot(futurePosition - wall.nominalPoint)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -29,3 +43,33 @@ func _process(delta):
 		translate(position_delta)
 	else:
 		speed = speed * -1 * abs(Wall.normalVector)
+
+func isMoving():
+	return inMovement
+
+func processCollisions():
+	for i in 3:
+		if(distancesFromWalls[i] < 0):
+			switchWallOnCollision(i)
+		else:
+			position[i] = futurePosition[i]
+
+func switchWallOnCollision(i):
+	speed[i] = -speed[i]
+	
+	var switchWallPlus
+	var switchWallMinus
+	if(i == 0):
+		switchWallPlus = allWalls[2]
+		switchWallMinus = allWalls[3]
+	elif(i == 1):
+		switchWallPlus = allWalls[1]
+		switchWallMinus = allWalls[0]
+	else:
+		switchWallPlus = allWalls[5]
+		switchWallMinus = allWalls[4]
+	
+	if(speed[i] < 0):
+		wallsGettingNearer[i] = switchWallMinus
+	else:
+		wallsGettingNearer[i] = switchWallPlus
--- pool_area.gd
+++ pool_area.gd
@@ -1,14 +1,49 @@
 extends Node3D
 
-var ball := load("res://node3dBall.tscn") as PackedScene;
+var ballClass := load("res://node3dBall.tscn") as PackedScene;
+
+var balls := []
+@onready var wall1 := $Room/Wall1
+@onready var wall2 := $Room/Wall2
+@onready var wall3 := $Room/Wall3
+@onready var wall4 := $Room/Wall4
+@onready var wall5 := $Room/Wall5
+@onready var wall6 := $Room/Wall6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var ball1 = ball.instantiate()
-	ball1.position = Vector3(-6.5, 0., 0.)
+	var ball1 = ballClass.instantiate()
+	ball1.init(Vector3(-6.5, 0., 0.), [wall1, wall2, wall3, wall4, wall5, wall6])
 	add_child(ball1)
+	balls.append(ball1)
+	
+	var timer := Timer.new()
+	add_child(timer)
+	
+	timer.wait_time = 3.0
+	timer.one_shot = true
+	timer.connect("timeout", triggerMovement)
+	timer.start()
+
+func triggerMovement():
+	print(wall1.normalVector)
+	balls[0].setMovement(Vector3(-1., -10., 40.), 
+		[wall3, wall1, wall6])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	for ball in balls:
+		if(ball.isMoving()):
+			countNewPositions(ball, delta)
+			findCollisions(ball)
+			processCollisions(ball)
+
+func countNewPositions(ball, delta):
+	ball.countNewPosition(delta)
+
+func findCollisions(ball):
+	ball.countWallDistances();
+
+func processCollisions(ball):
+	ball.processCollisions()
