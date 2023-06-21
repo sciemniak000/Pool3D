dc9eda93e082e51bf86ece45945800f8dc542203
sciemniak000 <sciemniak000@gmail.com>
2023-06-21 19:02:52 +0200

    Balls collisions with wrong angles

--- Node3DBall.gd
+++ Node3DBall.gd
@@ -8,9 +8,13 @@ var allWalls
 var wallsGettingNearer := [null, null, null]
 var distancesFromWalls := [1., 1., 1.]
 
+@onready var radiusContainer := $StaticBody3D/CollisionShape3D/CSGSphere3D
+var radius
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	speed = Vector3(0., 0., 0.)
+	radius = radiusContainer.radius
 
 func init(positioning, walls):
 	position = positioning
@@ -19,17 +23,18 @@ func init(positioning, walls):
 
 func setMovement(newSpeed):
 	speed = newSpeed
-	print("Speed set to: " + str(speed))
 	setWallsApproaching()
 	inMovement = true
 
 func countNewPosition(delta):
 	var position_delta := Vector3(delta * speed[0], delta * speed[1], delta * speed[2])
-	print("Calculated position delta to: " + str(position_delta) + " from delta: " + str(delta)
-	+ " and speed: " + str(speed))
 	for i in 3:
 		futurePosition[i] = position[i] + position_delta[i]
-	print("Future position set to: " + str(futurePosition))
+
+func countNewPositionWithPartialSpeed(delta, partialSpeed):
+	var position_delta := Vector3(delta * partialSpeed[0], delta * partialSpeed[1], delta * partialSpeed[2])
+	for i in 3:
+		futurePosition[i] = position[i] + position_delta[i]
 
 func countWallDistances():
 	for i in 3:
@@ -40,8 +45,7 @@ func countFutureDistanceToWall(wall):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("new position: " + str(futurePosition))
-	print(distancesFromWalls)
+	pass
 
 func isMoving():
 	return inMovement
@@ -49,33 +53,25 @@ func isMoving():
 func processCollisions():
 	for i in 3:
 		if(distancesFromWalls[i] < 0):
-			print("should collide with wall: " + wallsGettingNearer[i].getDebugId() + "; old speed: " + str(speed))
 			speed[i] = -speed[i]
-			print("new speed: " + str(speed))
 			futurePosition[i] = position[i]
 			setWallsApproaching()
 
 func setWallsApproaching():
 	if(speed[0] >= 0):
 		wallsGettingNearer[0] = allWalls[1]
-		print("Setting first awaiting wall to " + allWalls[0].getDebugId())
 	else:
 		wallsGettingNearer[0] = allWalls[0]
-		print("Setting first awaiting wall to " + allWalls[1].getDebugId())
 	
 	if(speed[1] >= 0):
 		wallsGettingNearer[1] = allWalls[3]
-		print("Setting second awaiting wall to " + allWalls[3].getDebugId())
 	else:
 		wallsGettingNearer[1] = allWalls[2]
-		print("Setting second awaiting wall to " + allWalls[2].getDebugId())
 	
 	if(speed[2] >= 0):
 		wallsGettingNearer[2] = allWalls[5]
-		print("Setting third awaiting wall to " + allWalls[5].getDebugId())
 	else:
 		wallsGettingNearer[2] = allWalls[4]
-		print("Setting third awaiting wall to " + allWalls[4].getDebugId())
 
 func acceptThePosition():
 	for i in 3:
--- pool_area.gd
+++ pool_area.gd
@@ -10,10 +10,13 @@ var balls := []
 @onready var wall5 := $Room/Wall5
 @onready var wall6 := $Room/Wall6
 
+var ballsCollidingLeft := []
+var ballsCollidingRight := []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	createBall(Vector3(-49., 0., 0.))
-	createBall(Vector3(3., 33., 45.))
+	createBall(Vector3(-49., -40., 0.25))
+	createBall(Vector3(-49., 40., 0.))
 	
 	var timer := Timer.new()
 	add_child(timer)
@@ -31,15 +34,15 @@ func createBall(positioning):
 	balls.append(ball)
 
 func triggerMovement():
-	balls[0].setMovement(Vector3(-40., 20., 10.))
-	balls[1].setMovement(Vector3(4., 48., 23.))
+	balls[0].setMovement(Vector3(0., 40., 0.))
+	balls[1].setMovement(Vector3(0., -40., 0.))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	countFuturePositions(delta)
 	findCollisions()
-	processCollisions()
+	processCollisions(delta)
 	proceedToPosition()
 
 func countFuturePositions(delta):
@@ -51,11 +54,52 @@ func findCollisions():
 	for ball in balls:
 		if ball.isMoving():
 			ball.countWallDistances();
+	
+	for i in range(balls.size()):
+		for j in range(i + 1, balls.size()):
+			var distance = sqrt(pow(balls[i].futurePosition[0] - balls[j].futurePosition[0], 2) 
+			+ pow(balls[i].futurePosition[1] - balls[j].futurePosition[1], 2)
+			+ pow(balls[i].futurePosition[2] - balls[j].futurePosition[2], 2))
+			if distance <= balls[i].radius + balls[j].radius:
+				ballsCollidingLeft.append(balls[i])
+				ballsCollidingRight.append(balls[j])
 
-func processCollisions():
+func processCollisions(delta):
 	for ball in balls:
 		if ball.isMoving():
 			ball.processCollisions()
+	
+	for i in range(min(ballsCollidingLeft.size(), ballsCollidingRight.size())):
+		var ball1 = ballsCollidingLeft[i]
+		var ball2 = ballsCollidingRight[i]
+		var normalVector = getNormalVector(ball1, ball2)
+		print("normal vector calculated to: " + str(normalVector))
+		var collisionSpeed1 = countCollisionSpeed(normalVector, ball1)
+		var collisionSpeed2 = countCollisionSpeed(normalVector, ball2)
+		print("collision vectors calculated 1 to: " + str(collisionSpeed1) + "; and 2 to: " + str(collisionSpeed2))
+		processBallsCollision(delta, ball1, ball2, collisionSpeed1, collisionSpeed2)
+	
+	ballsCollidingLeft.clear()
+	ballsCollidingRight.clear()
+
+func getNormalVector(ball1, ball2):
+	var pos1 = ball1.position
+	var pos2 = ball2.position
+	var dif = pos2 - pos1
+	return dif/sqrt(pow(dif[0], 2) + pow(dif[1], 2) + pow(dif[2], 2))
+
+func countCollisionSpeed(normalVector, ball):
+	return normalVector.dot(ball.speed)*normalVector
+
+func processBallsCollision(delta, ball1, ball2, collisionSpeed1, collisionSpeed2):
+	var unimpactedSpeed1 = ball1.speed - collisionSpeed1
+	var unimpactedSpeed2 = ball2.speed - collisionSpeed2
+	
+	ball1.countNewPositionWithPartialSpeed(delta, unimpactedSpeed1)
+	ball2.countNewPositionWithPartialSpeed(delta, unimpactedSpeed2)
+	
+	ball1.setMovement(unimpactedSpeed1 + collisionSpeed2)
+	ball2.setMovement(unimpactedSpeed2 + collisionSpeed1)
 
 func proceedToPosition():
 	for ball in balls:
